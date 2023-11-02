26d75f46e99c9433ea305c584e581f4331c80be5
Adrian Swiatek <adrian.swiatek@dazn.com>
2023-11-02 10:11:04 +0100

    Set condition to the Previous button

--- pdf-reader/Views/Content/ContentView+NoContent.swift
+++ pdf-reader/Views/Content/ContentView+NoContent.swift
@@ -1,12 +1,19 @@
+import SwiftData
 import SwiftUI
 
 extension ContentView {
     struct NoContentView: View {
+        @Query
+        private var bookProgresses: [BookProgress]
+
         @Binding
         private var isFilePickerShown: Bool
 
         @Binding
         private var arePreviousShown: Bool
+
+        @State
+        private var canShowPrevious: Bool
 
         init(
             _ isFilePickerShown: Binding<Bool>,
@@ -14,6 +21,7 @@ extension ContentView {
         ) {
             self._isFilePickerShown = isFilePickerShown
             self._arePreviousShown = arePreviousShown
+            self._canShowPrevious = State(wrappedValue: false)
         }
 
         var body: some View {
@@ -27,7 +35,7 @@ extension ContentView {
 
                 Text("No PDF document opened")
             } actions: {
-                HStack(alignment: .bottom, spacing: 32) {
+                HStack(alignment: .bottom, spacing: 64) {
                     Button {
                         isFilePickerShown.toggle()
                     } label: {
@@ -39,10 +47,10 @@ extension ContentView {
                                 .font(.title3)
                         }
                         .fontWeight(.medium)
-                        .foregroundColor(Color(uiColor: .label).opacity(0.75))
-                        .frame(maxHeight: .infinity, alignment: .bottom)
-                        .padding()
+                        .padding(.leading)
+                        .padding(.vertical)
                     }
+                    .buttonStyle(BaseButtonStyle().accented())
 
                     Button {
                         arePreviousShown.toggle()
@@ -55,11 +63,11 @@ extension ContentView {
                             Text("Previous")
                                 .font(.title3)
                         }
-                        .fontWeight(.regular)
-                        .foregroundColor(Color(uiColor: .label).opacity(0.75))
-                        .frame(maxHeight: .infinity, alignment: .bottom)
-                        .padding()
+                        .padding(.trailing)
+                        .padding(.vertical)
                     }
+                    .disabled(!canShowPrevious)
+                    .buttonStyle(BaseButtonStyle(isActive: canShowPrevious))
                 }
                 .padding(.horizontal, 24)
                 .padding(.vertical, 8)
@@ -70,6 +78,12 @@ extension ContentView {
                 .padding()
             }
             .background(Color(uiColor: .systemBackground).gradient)
+            .onChange(of: bookProgresses, { _, _ in setCanShowPrevious() })
+            .onAppear(perform: setCanShowPrevious)
+        }
+
+        private func setCanShowPrevious() {
+            canShowPrevious = !bookProgresses.isEmpty
         }
     }
 }
